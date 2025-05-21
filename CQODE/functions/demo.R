c25e4a5fd5f7f2c0d98387e6083f9cb83bced860
deetherese <theresemaria98@gmail.com>
2025-05-21 16:02:48 -0400

    Initial setup #5

--- CQODE/functions/demo.R
+++
@@ -1,9 +0,0 @@
-# ===== demo() =====
-# Provides a message demonstrating the library has been imported correctly
-
-# Input: NA
-# Output: A greeting message for demo use
-
-demo <- function() {
-    return('Hi! This is a demo!')
-}
