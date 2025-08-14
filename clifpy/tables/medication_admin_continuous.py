e83747f717e681fa35cd23e6aeac970eb07514a8
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-08-14 16:47:16 -0500

    wip(mac): finish enhancing the med dose unit converter and add pytest placeholders for cc to implement

--- clifpy/tables/medication_admin_continuous.py
+++ clifpy/tables/medication_admin_continuous.py
@@ -1,6 +1,7 @@
 from ast import Set
-from typing import Optional, Dict
+from typing import Optional, Dict, Tuple, Union
 import pandas as pd
+from pyarrow import BooleanArray
 from .base_table import BaseTable
 import duckdb
 
@@ -64,16 +65,65 @@ class MedicationAdminContinuous(BaseTable):
     # Medication-specific methods can be added here if needed
     # The base functionality (validate, isvalid, from_file) is inherited from BaseTable
     
-    def _check_med_dose_unit_in_acceptable_pattern(self, med_df: pd.DataFrame = None) -> bool | Set[str]:
-        pass
+    @property
+    def _acceptable_dose_unit_patterns(self) -> Set[str]:
+        """
+        Returns a set of post-cleaning acceptable dose unit patterns (all in lowercase, no white space)
+        """
+        acceptable_amounts = {
+            "ml", "l", 
+            "milli-units", "units", 
+            "mcg", "mg", "ng"
+            }
+        acceptable_weights = {'/kg', ''}
+        acceptable_times = {'/h', '/hr', '/hour', '/m', '/min', '/minute'}
+        # find the cartesian product of the three sets
+        return {a + b + c for a in acceptable_amounts for b in acceptable_weights for c in acceptable_times}
+    
+    def _standardize_dose_unit_pattern(
+        self, med_df: Optional[pd.DataFrame] = None
+        ) -> Tuple[pd.DataFrame, Union[Dict, bool]]:
+        """
+        Standardize dose unit to a consistent, convertible pattern, e.g. 'mL/ hr' -> 'ml/hr'.
         
-    def standardize_dose_unit(self, vitals_df: pd.DataFrame, med_df: pd.DataFrame = None) -> pd.DataFrame:
+        - removes white space
+        - converts to lowercase
+        - uses self.df by default if no argument is provided
+        
+        Returns:
+            A tuple where the first element is the input df with an appended column 'med_dose_unit_clean'; 
+            the second element is either a dictionary of the unaccounted-for dose units and their counts 
+            if any; or False if no unaccounted-for dose units
+        """
+        if med_df is None:
+            med_df = self.df
+        if med_df is None:
+            raise ValueError("No data provided")
+        
+        med_df['med_dose_unit_clean'] = med_df['med_dose_unit'].str.lower().str.strip()
+        
+        # find any rows with unseen, unaccounted-for dose units which we do not know how to convert
+        mask = ~med_df['med_dose_unit_clean'].isin(self._acceptable_dose_unit_patterns)
+        unaccounted: pd.DataFrame = med_df[mask]
+        
+        if not unaccounted.empty:
+            unaccounted_unit_counts = unaccounted.value_counts(subset=['med_dose_unit_clean']).to_dict()
+            self.logger.warning(f"The following dose units are not accounted by the converter: {unaccounted_unit_counts}")
+            return med_df, unaccounted_unit_counts
+        
+        return med_df, False
+        
+    def convert_dose_to_same_units(self, vitals_df: pd.DataFrame, med_df: pd.DataFrame = None) -> pd.DataFrame:
         """
         Standardize everything to mcg/min; ml/min; or units/min.
-        Requires the following columns as input:
+        
+        Requires the following columns in the input `med_df`:
         - med_dose_unit: the original unit of the dose (case-insensitive)
         - med_dose: the original dose
         - weight_kg: the most recent weight of the patient
+        
+        Returns:
+            pd.DataFrame: The input `med_df` with appended columns 'med_dose_converted' and 'med_dose_unit_converted'
         """
         if med_df is None:
             med_df = self.df
@@ -81,11 +131,11 @@ class MedicationAdminContinuous(BaseTable):
             raise ValueError("No data provided")
         
         if 'weight_kg' not in med_df.columns:
+            self.logger.info("No weight_kg column found, adding the most recent from vitals")
             query = """
             SELECT m.*
                 , v.vital_value as weight_kg
                 , v.recorded_dttm as weight_recorded_dttm
-                -- rn = 1 for the weight w/ the latest recorded_dttm (and thus most recent)
                 , ROW_NUMBER() OVER (
                     PARTITION BY m.hospitalization_id, m.admin_dttm, m.med_category
                     ORDER BY v.recorded_dttm DESC
@@ -95,7 +145,8 @@ class MedicationAdminContinuous(BaseTable):
                 ON m.hospitalization_id = v.hospitalization_id 
                 AND v.vital_category = 'weight_kg' AND v.vital_value IS NOT NULL
                 AND v.recorded_dttm <= m.admin_dttm  -- only past weights
-            QUALIFY (rn = 1) -- OR (weight_kg IS NULL) -- include meds even if no weight found
+            -- rn = 1 for the weight w/ the latest recorded_dttm (and thus most recent)
+            QUALIFY (rn = 1) 
             ORDER BY m.hospitalization_id, m.admin_dttm, m.med_category, rn
             """
             med_df = duckdb.sql(query).to_df()
@@ -104,28 +155,36 @@ class MedicationAdminContinuous(BaseTable):
         required_columns = {'med_dose_unit', 'med_dose', 'weight_kg'}
         missing_columns = required_columns - set(med_df.columns)
         if missing_columns:
-            raise ValueError(f"The following columns are required: {missing_columns}")
+            raise ValueError(f"The following column(s) are required but not found: {missing_columns}")
+        
+        med_df, unaccounted = self._standardize_dose_unit_pattern(med_df)
+        if not unaccounted:
+            self.logger.info("No unaccounted-for dose units found, continuing with conversion")
+        else:
+            self.logger.warning(f"Unaccounted-for dose units found: {unaccounted}")
         
         query = f"""
         SELECT *
-            , LOWER(med_dose_unit) AS med_dose_unit_lower
-            , CASE WHEN regexp_matches(med_dose_unit_lower, '/h(r|our)?\\b') THEN 1/60.0
-                WHEN regexp_matches(med_dose_unit_lower, '/m(in|inute)?\\b') THEN 1.0
+            , CASE WHEN regexp_matches(med_dose_unit_clean, '/h(r|our)?\\b') THEN 1/60.0
+                WHEN regexp_matches(med_dose_unit_clean, '/m(in|inute)?\\b') THEN 1.0
                 ELSE NULL END as time_multiplier
-            , CASE WHEN contains(med_dose_unit_lower, '/kg/') THEN weight_kg
-                ELSE 1 END AS pt_weight_adjustment
-            , CASE WHEN contains(med_dose_unit_lower, 'mcg/') THEN 1.0
-                WHEN contains(med_dose_unit_lower, 'mg/') THEN 1000.0
-                WHEN contains(med_dose_unit_lower, 'ng/') THEN 0.001
-                WHEN contains(med_dose_unit_lower, 'milli') THEN 0.001
-                WHEN contains(med_dose_unit_lower, 'units/') THEN 1
-                WHEN contains(med_dose_unit_lower, 'ml/') THEN 1.0
-                WHEN contains(med_dose_unit_lower, 'l/') AND NOT contains(med_dose_unit_lower, 'ml/') THEN 1000.0
-                ELSE NULL END as dose_multiplier
-            , med_dose * time_multiplier * pt_weight_adjustment * dose_multiplier as med_dose_converted
-            , CASE WHEN contains(med_dose_unit_lower, 'units/') THEN 'units/min'
-                WHEN contains(med_dose_unit_lower, 'l/') THEN 'ml/min'
+            , CASE WHEN contains(med_dose_unit_clean, '/kg/') THEN weight_kg
+                ELSE 1 END AS pt_weight_multiplier
+            , CASE WHEN contains(med_dose_unit_clean, 'mcg/') THEN 1.0
+                WHEN contains(med_dose_unit_clean, 'mg/') THEN 1000.0
+                WHEN contains(med_dose_unit_clean, 'ng/') THEN 0.001
+                WHEN contains(med_dose_unit_clean, 'milli') THEN 0.001
+                WHEN contains(med_dose_unit_clean, 'units/') THEN 1
+                WHEN contains(med_dose_unit_clean, 'ml/') THEN 1.0
+                WHEN contains(med_dose_unit_clean, 'l/') AND NOT contains(med_dose_unit_clean, 'ml/') THEN 1000.0
+                ELSE NULL END as amount_multiplier
+            , med_dose * time_multiplier * pt_weight_multiplier * amount_multiplier as med_dose_converted
+            , CASE WHEN contains(med_dose_unit_clean, 'units/') THEN 'units/min'
+                WHEN contains(med_dose_unit_clean, 'l/') THEN 'ml/min'
                 ELSE 'mcg/min' END as med_dose_unit_converted
         FROM med_df
         """
         return duckdb.sql(query).to_df()
+    
+    
+    
