cf46427cbc5befc95bbf670e351fc573f042467b
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-08-14 12:28:35 -0500

    feat(mac): add universal med dose converter to the table class and simple notebook-based tests

--- clifpy/tables/medication_admin_continuous.py
+++ clifpy/tables/medication_admin_continuous.py
@@ -1,7 +1,8 @@
+from ast import Set
 from typing import Optional, Dict
 import pandas as pd
 from .base_table import BaseTable
-
+import duckdb
 
 class MedicationAdminContinuous(BaseTable):
     """
@@ -62,3 +63,69 @@ class MedicationAdminContinuous(BaseTable):
     
     # Medication-specific methods can be added here if needed
     # The base functionality (validate, isvalid, from_file) is inherited from BaseTable
+    
+    def _check_med_dose_unit_in_acceptable_pattern(self, med_df: pd.DataFrame = None) -> bool | Set[str]:
+        pass
+        
+    def standardize_dose_unit(self, vitals_df: pd.DataFrame, med_df: pd.DataFrame = None) -> pd.DataFrame:
+        """
+        Standardize everything to mcg/min; ml/min; or units/min.
+        Requires the following columns as input:
+        - med_dose_unit: the original unit of the dose (case-insensitive)
+        - med_dose: the original dose
+        - weight_kg: the most recent weight of the patient
+        """
+        if med_df is None:
+            med_df = self.df
+        if med_df is None:
+            raise ValueError("No data provided")
+        
+        if 'weight_kg' not in med_df.columns:
+            query = """
+            SELECT m.*
+                , v.vital_value as weight_kg
+                , v.recorded_dttm as weight_recorded_dttm
+                -- rn = 1 for the weight w/ the latest recorded_dttm (and thus most recent)
+                , ROW_NUMBER() OVER (
+                    PARTITION BY m.hospitalization_id, m.admin_dttm, m.med_category
+                    ORDER BY v.recorded_dttm DESC
+                    ) as rn
+            FROM med_df m
+            LEFT JOIN vitals_df v 
+                ON m.hospitalization_id = v.hospitalization_id 
+                AND v.vital_category = 'weight_kg' AND v.vital_value IS NOT NULL
+                AND v.recorded_dttm <= m.admin_dttm  -- only past weights
+            QUALIFY (rn = 1) -- OR (weight_kg IS NULL) -- include meds even if no weight found
+            ORDER BY m.hospitalization_id, m.admin_dttm, m.med_category, rn
+            """
+            med_df = duckdb.sql(query).to_df()
+        
+        # check if the required columns are present
+        required_columns = {'med_dose_unit', 'med_dose', 'weight_kg'}
+        missing_columns = required_columns - set(med_df.columns)
+        if missing_columns:
+            raise ValueError(f"The following columns are required: {missing_columns}")
+        
+        query = f"""
+        SELECT *
+            , LOWER(med_dose_unit) AS med_dose_unit_lower
+            , CASE WHEN regexp_matches(med_dose_unit_lower, '/h(r|our)?\\b') THEN 1/60.0
+                WHEN regexp_matches(med_dose_unit_lower, '/m(in|inute)?\\b') THEN 1.0
+                ELSE NULL END as time_multiplier
+            , CASE WHEN contains(med_dose_unit_lower, '/kg/') THEN weight_kg
+                ELSE 1 END AS pt_weight_adjustment
+            , CASE WHEN contains(med_dose_unit_lower, 'mcg/') THEN 1.0
+                WHEN contains(med_dose_unit_lower, 'mg/') THEN 1000.0
+                WHEN contains(med_dose_unit_lower, 'ng/') THEN 0.001
+                WHEN contains(med_dose_unit_lower, 'milli') THEN 0.001
+                WHEN contains(med_dose_unit_lower, 'units/') THEN 1
+                WHEN contains(med_dose_unit_lower, 'ml/') THEN 1.0
+                WHEN contains(med_dose_unit_lower, 'l/') AND NOT contains(med_dose_unit_lower, 'ml/') THEN 1000.0
+                ELSE NULL END as dose_multiplier
+            , med_dose * time_multiplier * pt_weight_adjustment * dose_multiplier as med_dose_converted
+            , CASE WHEN contains(med_dose_unit_lower, 'units/') THEN 'units/min'
+                WHEN contains(med_dose_unit_lower, 'l/') THEN 'ml/min'
+                ELSE 'mcg/min' END as med_dose_unit_converted
+        FROM med_df
+        """
+        return duckdb.sql(query).to_df()
