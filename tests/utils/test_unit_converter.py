f0a9ee7564faef0a1028a34032edb27991cc8449
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-08-27 19:28:37 -0500

    test(meds): build and pass the test for _detect_and_classify_normalized_dose_units()

--- tests/utils/test_unit_converter.py
+++ tests/utils/test_unit_converter.py
@@ -7,7 +7,9 @@ import numpy as np
 from pathlib import Path
 from clifpy.utils.unit_converter import (
     _normalize_dose_unit_formats,
-    _normalize_dose_unit_names
+    _normalize_dose_unit_names,
+    _detect_and_classify_normalized_dose_units,
+    ACCEPTABLE_RATE_UNITS
 )
 
 # --- Helper Fixtures for CSV Loading ---
@@ -59,3 +61,48 @@ def test_normalize_dose_unit_names(_normalize_dose_unit_names_test_data):
         test_df['med_dose_unit_name_normalized'].reset_index(drop=True),
         check_names=False
     )
+
+def test_acceptable_rate_units():
+    positive_cases = ['ml/hr', 'ml/min', 'l/hr', 'l/min', 'u/hr', 'u/min']
+    negative_cases = ['ml', 'l', 'u', 'mcg', 'mg', 'ng', 'units/min']
+    
+    for unit in positive_cases:
+        assert unit in ACCEPTABLE_RATE_UNITS
+    
+    for unit in negative_cases:
+        assert unit not in ACCEPTABLE_RATE_UNITS
+ 
+def test_detect_and_classify_normalized_dose_units():
+    test_series = pd.Series([
+        # rate units
+        'u/min', 
+        'ml/hr', 'ml/hr', 
+        'mcg/kg/hr', 'mcg/kg/hr', 'mcg/kg/hr',
+        # amount units
+        'ml',
+        'mu', 'mu',
+        'g', 'g', 'g',
+        # unrecognized units
+        'units/min', 
+        'kg', 'kg', 
+        pd.NA, pd.NA, 
+        None, None, None,
+        "", "", "", "",
+        np.nan, np.nan, np.nan, np.nan, np.nan
+    ])
+    
+    expected_dict = {
+        'rate_units': {
+            'u/min': 1, 'ml/hr': 2, 'mcg/kg/hr': 3
+        },
+        'amount_units': {
+            'ml': 1, 'mu': 2, 'g': 3
+        },
+        'unrecognized_units': {
+            'units/min': 1, 'kg': 2, pd.NA: 2, None: 3, '': 4, np.nan: 5
+        }
+    }
+    
+    result_dict = _detect_and_classify_normalized_dose_units(test_series)
+    
+    assert result_dict == expected_dict
